c2610dfe473dbf42af5e1f18ee51d8a2f86273b1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-09 09:53:29 +0100

    Update commit

--- store/__init__.py
+++ store/__init__.py
@@ -1,7 +1,22 @@
 from flask import Flask
 from store.config import DevelopmentConfig
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
 
-from store import views
+Bootstrap(app)
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+login_manager.login_message = "Log in to access this page."
+login_manager.login_message_category = "danger"
+
+from store import views, models
