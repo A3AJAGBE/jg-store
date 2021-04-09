4f25cbb6db53ced28d7a150a3c3c0c049391c6a0
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-09 09:53:03 +0100

    Add flask login manager

--- store/views.py
+++ store/views.py
@@ -1,10 +1,16 @@
 from flask import render_template
 from datetime import datetime
-from store import app
-from store.forms import *
+from store import app, login_manager
+from store.forms import RegisterForm, LoginForm
+from store.models import Users
 
 # Get the year
 current_year = datetime.now().year
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
 
 
 @app.route('/')
@@ -12,7 +18,7 @@ def index():
     return render_template('index.html', year=current_year)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
     if form.validate_on_submit():
@@ -20,7 +26,7 @@ def register():
     return render_template('register.html', year=current_year, form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
