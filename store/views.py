86d6e7e8148ca6fceee9b045d81d23aa5c1d67d1
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-09 10:19:10 +0100

    Add login form validation and logout view

--- store/views.py
+++ store/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
-from flask_login import current_user, login_user
-from werkzeug.security import generate_password_hash
+from flask_login import current_user, login_user, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from datetime import datetime
 from store import app, db, login_manager
 from store.forms import RegisterForm, LoginForm
@@ -57,6 +57,29 @@ def register():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if current_user.is_authenticated:
+        flash("You are unable to view that page because you are currently logged in.", "info")
+        return redirect(url_for('index'))
     if form.validate_on_submit():
-        pass
+        email = request.form['email']
+        password = request.form['password']
+
+        # Find the user by email
+        user = Users.query.filter_by(email=email).first()
+
+        if not user:
+            flash("Invalid email address, try again.", "danger")
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, try again.', "danger")
+        else:
+            login_user(user)
+            flash('You are logged in successfully', "success")
+            return redirect(url_for('index'))
     return render_template('login.html', year=current_year, form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('You are logged out successfully', "success")
+    return redirect(url_for('index'))
