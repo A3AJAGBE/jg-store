b2a61571420039c370d26e61e3aae6e285761ce0
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-09 08:51:24 +0100

    Add register and login view'

--- store/views.py
+++ store/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from datetime import datetime
 from store import app
+from store.forms import *
 
 # Get the year
 current_year = datetime.now().year
@@ -9,3 +10,19 @@ current_year = datetime.now().year
 @app.route('/')
 def index():
     return render_template('index.html', year=current_year)
+
+
+@app.route('/register')
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('register.html', year=current_year, form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass
+    return render_template('login.html', year=current_year, form=form)
