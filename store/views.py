061692d8c59c51107faf0740ed65a51a5fea3088
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-09 10:13:30 +0100

    Add register form validation

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user
+from werkzeug.security import generate_password_hash
 from datetime import datetime
-from store import app, login_manager
+from store import app, db, login_manager
 from store.forms import RegisterForm, LoginForm
 from store.models import Users
 
@@ -21,8 +23,34 @@ def index():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
+    if current_user.is_authenticated:
+        flash("You are unable to view that page because you are currently logged in.", "info")
+        return redirect(url_for('index'))
     if form.validate_on_submit():
-        pass
+        first_name = request.form['first_name'].title()
+        last_name = request.form['last_name'].title()
+        email = request.form['email']
+        password = request.form['password']
+
+        # Make sure the email does not exist
+        if Users.query.filter_by(email=email).first():
+            flash("That email exists in the database.", "danger")
+        else:
+            encrypt_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=8)
+
+            new_user = Users(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                password=encrypt_password,
+            )
+            db.session.add(new_user)
+            db.session.commit()
+
+            # Log in and authenticate user after adding details to database.
+            login_user(new_user)
+            flash(f"Registered successfully {first_name}, you're now logged in.", "success")
+            return redirect(url_for('index'))
     return render_template('register.html', year=current_year, form=form)
 
 
