9244252b79badce03bf6d288235dba37580fc6a7
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-09 08:28:36 +0100

    Add app views

--- store/views.py
+++ store/views.py
@@ -1,6 +1,11 @@
+from flask import render_template
+from datetime import datetime
 from store import app
+
+# Get the year
+current_year = datetime.now().year
 
 
 @app.route('/')
 def index():
-    return 'The Jewelry Gallery'
+    return render_template('index.html', year=current_year)
