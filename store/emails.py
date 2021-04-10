377aab7e3918e8893e70c4eadd05b1ce5a7d8c3e
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-10 15:20:24 +0100

    Update commit

--- store/emails.py
+++ store/emails.py
@@ -1,9 +1,36 @@
 from flask_mail import Message
-from store import mail
+from store import mail, app
+from itsdangerous import URLSafeTimedSerializer
 
 
-def send_email(subject, sender, recipients, text_body, html_body):
-    msg = Message(subject, sender=sender, recipients=recipients)
-    msg.body = text_body
-    msg.html = html_body
+def generate_token(email):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    return serializer.dumps(email, salt=app.config['SECURITY_SALT'])
+
+
+def confirm_token(token, expiration=1800):
+    serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
+    email = serializer.loads(
+        token,
+        salt=app.config['SECURITY_SALT'],
+        max_age=expiration
+    )
+    return email
+
+
+def send_email(subject, recipients, text_body, html_body):
+    msg = Message(
+        subject,
+        sender=app.config['MAIL_DEFAULT_SENDER'],
+        recipients=recipients,
+        body=text_body,
+        html=html_body,
+    )
     mail.send(msg)
+
+
+def email_confirmation(user_email, text_body, html_body):
+    send_email('[The Jewelry Gallery] Email Confirmation Request',
+               recipients=[user_email],
+               text_body=text_body,
+               html_body=html_body)
