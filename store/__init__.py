b6f2c05a3b0a1fdbdd130841b263dbed04dce9c7
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-10 11:20:11 +0100

    Update commit

--- store/__init__.py
+++ store/__init__.py
@@ -4,6 +4,7 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 import logging
 from logging.handlers import SMTPHandler
 
@@ -14,6 +15,8 @@ Bootstrap(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+mail = Mail(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
