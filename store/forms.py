5fad48331adc03044ac1b49b0ae3f7ee3e578279
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-11 08:42:49 +0100

    Add contact form

--- store/forms.py
+++ store/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField, validators, BooleanField
+from wtforms import StringField, SubmitField, PasswordField, validators, TextAreaField
 
 
 class RegisterForm(FlaskForm):
@@ -28,20 +28,20 @@ class LoginForm(FlaskForm):
 class NewEmailConfirmationRequestForm(FlaskForm):
     email = StringField("Registered Email Address", [validators.InputRequired(message="This field cannot be empty."),
                                                      validators.Email(message="That is not a valid email address.")])
-    submit = SubmitField("Submit Request")
+    submit = SubmitField("Submit request")
 
 
 class ResetPasswordRequestForm(FlaskForm):
     email = StringField("Registered Email Address", [validators.InputRequired(message="This field cannot be empty."),
                                                      validators.Email(message="That is not a valid email address.")])
-    submit = SubmitField("Submit Request")
+    submit = SubmitField("Submit request")
 
 
 class ResetPasswordForm(FlaskForm):
     password = PasswordField("New Password", [validators.InputRequired(message="This field cannot be empty."),
                                               validators.Length(min=8,
                                                                 message="Password must be at least 8 characters.")])
-    submit = SubmitField("Reset Password")
+    submit = SubmitField("Reset password")
 
 
 class ChangePasswordForm(FlaskForm):
@@ -51,4 +51,17 @@ class ChangePasswordForm(FlaskForm):
     new_password = PasswordField("New Password", [validators.InputRequired(message="This field cannot be empty."),
                                                   validators.Length(min=8,
                                                                     message="Password must be at least 8 characters.")])
-    submit = SubmitField("Change Password")
+    submit = SubmitField("Change password")
+
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', [validators.InputRequired(message="This field cannot be empty."),
+                                validators.Length(min=5, max=100,
+                                                  message="Name must be between 5 to 100 characters.")])
+    email = StringField('Email Address', [validators.InputRequired(message="This field cannot be empty."),
+                                          validators.Email(message="That is not a valid email address.")])
+    message = TextAreaField('Message', [validators.InputRequired(message="This field cannot be empty."),
+                                        validators.Length(min=10,
+                                                          message="Message must be at least 10 characters.")
+                                        ])
+    submit = SubmitField('Send message')
