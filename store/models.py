848e4993b5e39c2811d8180aded2392b7d2dc77a
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-04-12 09:42:37 +0100

    Add new models

--- store/models.py
+++ store/models.py
@@ -34,3 +34,38 @@ class Contact(db.Model):
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), nullable=False)
     message = db.Column(db.Text, nullable=False)
+
+
+class Categories(db.Model):
+    """Categories table"""
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(50), unique=True, nullable=False)
+    products = db.relationship('Products', backref='category', lazy=True)
+
+    def __repr__(self):
+        return self.category
+
+
+class Subcategories(db.Model):
+    """Sub Categories table"""
+    id = db.Column(db.Integer, primary_key=True)
+    sub_category = db.Column(db.String(50), unique=True, nullable=False)
+    products = db.relationship('Products', backref='subcategory', lazy=True)
+
+    def __repr__(self):
+        return self.sub_category
+
+
+class Products(db.Model):
+    """Products table"""
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    discount = db.Column(db.Integer)
+    stock = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String(128), unique=True, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), nullable=False)
+    sub_category_id = db.Column(db.Integer, db.ForeignKey('subcategories.id'), nullable=False)
+
+    def __repr__(self):
+        return self.name
